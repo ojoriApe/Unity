6789ef407e716c786a3bb48d3c00d99da68e26b3
ojoriApe <67466049+ojoriApe@users.noreply.github.com>
2020-07-06 09:57:35 +0900

    practice
    dd

---
+++ escape/Assets/Script/Ground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private Rigidbody2D groundRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag =="Needle" && !GameManager.instance.isGameover)
+        {
+            GameManager.instance.AddScore(1);
+        }
+    }
+}
