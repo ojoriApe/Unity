84bbee39d4d55f81387c0ea9ebec36ed87a72f09
ojoriApe <67466049+ojoriApe@users.noreply.github.com>
2020-07-06 09:55:06 +0900

    practice

--- escape/Assets/Script/Needle.cs
+++ escape/Assets/Script/Needle.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class Needle : MonoBehaviour
 {
-
+    public GameObject[] needles; //장애물들
+    
     private Rigidbody2D needleRigidbody;
-    // Start is called before the first frame update
+
+    //컴포넌트가 활성화될 때마다 매번 실행되는 메서드
+    private void OnEnable()
+    {
+        for (int i =0; i<needles.Length;i++)
+        {
+            if(Random.Range(0,3)==0)
+            {
+                needles[i].SetActive(true);
+            }
+            else
+            {
+                needles[i].SetActive(false);
+            }
+                
+        }
+    }
+
     void Start()
     {
         
@@ -24,7 +42,7 @@ public class Needle : MonoBehaviour
         {
             //상대방의 게임 오브젝트에서 PlayerController 컴포넌트 가져오기
             PlayerController playerController = other.GetComponent<PlayerController>();
-            Debug.Log("Die");
+            
             if(playerController != null)
             {
                 //상대방 PlayerController 컴포턴트의 Die() 메서드 실행
@@ -32,5 +50,6 @@ public class Needle : MonoBehaviour
                 
             }
         }
+    
     }
 }
